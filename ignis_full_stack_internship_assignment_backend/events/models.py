71223a2c5b67914401633163e0a2bbd674e4d0fa
Anubhab Dey <anubhabdey2017@gmail.com>
2024-04-12 22:32:45 +0530

    Added `Event` model

--- ignis_full_stack_internship_assignment_backend/events/models.py
+++ ignis_full_stack_internship_assignment_backend/events/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="events"
+    )
+    event_name = models.CharField(max_length=255)
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to="event_images/")
+    is_liked = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.event_name
