d3f7586afaf1c04ff57397b263478012de56587d
Anubhab Dey <anubhabdey2017@gmail.com>
2024-04-13 10:16:02 +0530

    Formatted

--- ignis_full_stack_internship_assignment_backend/ignis_full_stack_internship_assignment_backend/settings.py
+++ ignis_full_stack_internship_assignment_backend/ignis_full_stack_internship_assignment_backend/settings.py
@@ -144,7 +144,7 @@ DATABASES = {
         "NAME": "ignis_full_stack_internship_assignment_backend_database",
         "USER": "anubhabdey2017",
         "PASSWORD": "Anubhabdey@2017",
-        "HOST": "localhost",  # Or an IP Address that your DB is hosted on
-        "PORT": "3306",  # Default MySQL port
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
